c79b67d62c9efb3c8fa0221a222febe1c8b3f37a
arocanestor <aroca.nesror@gmail.com>
2020-09-02 21:01:52 -0500

    borrar

--- src/database.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://admin:1234@cluster0.18wxo.mongodb.net/ejem1?retryWrites=true&w=majority',{
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-    .then(db => console.log('database is conected'))
-    .catch(err => console.log(err));
